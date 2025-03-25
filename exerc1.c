a3e9ca461c033b2931d584444422809e1208cb31
Miguel <124186796+MiguelMendonSilva@users.noreply.github.com>
2025-03-25 19:53:02 -0300

    Update exerc1.c

--- exerc1.c
+++ exerc1.c
@@ -2,14 +2,14 @@
 
 int main() {
     int num1;
-    
+    int soma = 0;
     printf("Digite um número: ");
     scanf("%d", &num1);
 
     for(int num = 1 ; num <= num1; num+=2){
-        
         printf("%d\n", num);
+        soma +=num;
     }
-
+    printf("A soma dos numeros impares é: %d", soma);
   return 0;
 }  
